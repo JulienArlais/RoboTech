5347cfe656b6d65f88fc7fe6e62ad9c914410c43
David Lin <lindavid130@gmail.com>
2023-01-28 16:47:18 +0100

    Version David

--- Semaine 2.py
+++ Semaine 2.py
@@ -1,5 +1,4 @@
 ﻿import getch
-import math
 import numpy as np
 
 
@@ -28,103 +27,108 @@ class Object:
 
 
 class Robot:
-	def __init__(self, x, y, direction, vitesse, rayon):
+	def __init__(self, x, y, theta, vitesse, rayon):
 		# x et y des coordonnées en mètre, direction un angle par rapport à l'abscisse en float et la vitesse max du robot en m/s
 		# on suppose que le robot est un cercle, pour faciliter les collisions
 		self.x = x
 		self.y = y
-		self.direction = direction
+		self.theta = theta
 		self.vitesse = vitesse
 		self.rayon = rayon
-		print("Le robot de rayon",self.rayon,"est aux coordonnées (",self.x,",",self.y,"), a une direction d'angle",self.direction,"degrés et une vitesse maximale de",self.vitesse)
+
+	def avancer(self):
+		#fait avancer le robot pendant 1 seconde
+		self.x += self.vitesse*np.cos(robot.theta)
+		self.y += self.vitesse*np.sin(robot.theta)
+		
+
+	def tourner(self, angle):
+		# angle est la valeur d'angle que l'on va ajouter à notre angle. Elle peut être positive ou négative
+		self.theta += angle
+		while (self.theta < 0):
+			self.theta = self.theta + 360
+		self.theta = self.theta % 360
+		#print("La nouvelle direction du robot est de", self.theta, "degrés par rapport à l'axe des abscisses")
+
+	def reculer(self):
+		#fait avancer le robot pendant 1 seconde
+		self.x -= self.vitesse*np.cos(robot.theta)
+		self.y -= self.vitesse*np.sin(robot.theta)
+
 
 class Environnement:
-	def __init__(self, large, long, echelle): 
-        # l'échelle donne la longueur des côtés d'une case de l'environnement
-		self.largeur = large
-		self.longueur = long
-		self.grid = [[' ' for _ in range(large+2)] for _ in range(long+2)]
-		self.echelle = echelle
-		for i in range(long+1):
-				for j in range(large+1):
-					#self.grid[i][j] = '.'
-					if i == 0 or i == long-1:
-						self.grid[i][j] = chr(64+j)
-					elif j == 0 or j == large-1:
-						self.grid[i][j] = chr(64+i)
-					else:
-						self.grid[i][j] = " "
+	def __init__(self, width, height, scale): 
+		# prendre width et height >= 3 
+		self.width = width
+		self.height = height
+		self.scale = scale
+		self.grid = [[' ' for _ in range(width)] for _ in range(height)]
 
 class Simulation:
-	def tourner_robot(robot,angle):
-		# angle est la valeur d'angle que l'on va ajouter à notre angle. Elle peut être positive ou négative
-		robot.direction = robot.direction + angle
-		while (robot.direction < 0):
-			robot.direction = robot.direction + 360
-		robot.direction = robot.direction % 360
-		print("La nouvelle direction du robot est de",robot.direction,"degrés par rapport à l'axe des abscisses")
-		
-	def avancer_robot(environnement,robot):
-		#fait avancer le robot pendant 1 seconde
-		Simulation.retirer_robot_env(environnement,robot)
-		robot.x = robot.x + robot.vitesse*math.cos(robot.direction)
-		robot.y = robot.y + robot.vitesse*math.sin(robot.direction)
-		Simulation.placer_robot_env(environnement,robot)
-		print("Le robot a avancé et s'est déplacé en (",robot.x,",",robot.y,")")
-	
-	def reculer_robot(environnement,robot):
-		#fait avancer le robot pendant 1 seconde
-		robot.x = robot.x - robot.vitesse*math.cos(robot.direction)
-		robot.y = robot.y - robot.vitesse*math.sin(robot.direction)
-		Simulation.placer_robot_env(environnement,robot)
-		print("Le robot a reculé et s'est déplacé en (",robot.x,",",robot.y,")")
-	
+
 	def afficher_env(env):
-		for i in range(env.longueur):
-			for j in range(env.largeur):
+		for _ in range(env.width+2):
+			print('X', end=' ')
+		print()
+		for i in range(env.height):
+			print('X', end=' ')
+			for j in range(env.width):
 				print(env.grid[i][j], end=' ')
-			print()
-	
+			print('X')
+		for _ in range(env.width+2):
+			print('X', end=' ')	
+		print()
+
 	def retirer_robot_env(env,robot):
-		x = int(robot.x / env.echelle)
-		y = int(robot.y / env.echelle)
-		if (env.grid[y][x] == "R"):
-			env.grid[y][x] = "."
-		elif (env.grid[y][x] == "#"):
-			env.grid[y][x] = "@"
+		x = int(robot.x / env.scale)
+		y = int(robot.y / env.scale)
+		if (env.grid[y][x] == 'R'):
+			env.grid[y][x] = '.'
 	
 	def placer_robot_env(env, robot):
-		x = int(robot.x / env.echelle)
-		y = int(robot.y / env.echelle)
-		if (env.grid[y][x] == " " or env.grid[y][x] == "."):
-			env.grid[y][x] = "R"
-		elif (env.grid[y][x] == "@"):
-			env.grid[y][x] = "#"
-		#print("Le robot a été placé à ses coordonnées dans l'environnement")
+		x = int(robot.x / env.scale)
+		y = int(robot.y / env.scale)
+		if (env.grid[y][x] == ' ' or env.grid[y][x] == '.'):
+			env.grid[y][x] = 'R'
 
-ech = float(input("Entrez l'échelle de l'environnement. Chaque case aura une largeur et longueur en mètre égale à cette valeur: "))
-while (ech < 1):
-	ech = float(input("L'échelle doit au moins être de 1 mètre: "))
-large = int(input("Entrez la largeur (abscisse) de l'environnement: "))
-while (large < 0):
-	large = int(input("La largeur doit être une valeur positive. Entrez à nouveau: "))
-long = int(input("Entrez la longueur (abscisse) de l'environnement: "))
-while (long < 0):
-	long = int(input("La longueur doit être une valeur positive. Entrez à nouveau: "))
+	def avancer_robot_env(env, robot):
+		env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] = ' '
+		robot.avancer()
+		if (env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] != 'X'):
+			env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] = 'R'
+			print("Le robot a avancé et s'est déplacé en (",robot.x,",",robot.y,")")
+		else:
+			print("Collision")
 
-environnement = Environnement(large,long,ech)
-print("L'environnement fait",environnement.largeur,"case de large et",environnement.longueur,"de long, et chaque case fait",environnement.echelle,"*",environnement.echelle,"mètres de côté\n")
+	def reculer_robot_env(env, robot):
+		env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] = ' '
+		robot.reculer()
+		if (env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] != 'X'):
+			env.grid[int(robot.y / env.scale)][int(robot.x / env.scale)] = 'R'
+			print("Le robot a reculé et s'est déplacé en (",robot.x,",",robot.y,")")
+		else:
+			print("Collision")
 
-x = float(input("Entrez l'abscisse initiale du robot: "))
-y = float(input("Entrez l'ordonnée initiale du robot: "))
-direction = float(input("Entrez l'angle du robot par rapport à l'abscisse: "))
-vitesse = float(input("Entrez la vitesse maximale du robot: "))
-rayon = float(input("Entrez le rayon du robot: "))
-robot = Robot(x,y,direction,vitesse,rayon)
+	def distance(x1, y1, x2, y2): 
+		dist = np.sqrt(np.square(x2 - x1)+np.square(y2 - y1))
+		print("Distance entre (", x1, ",", y1, ") et (", x2, ",", y2, ") :", dist)
+
+
+
+
+
+environnement = Environnement(20, 10, 1)
+print("L'environnement fait",environnement.width,"case de large et",environnement.height,"de long, et chaque case fait",environnement.scale,"mètres de côté\n")
+
+robot = Robot(9.9, 1.1, 0, 1, 1)
 
 Simulation.placer_robot_env(environnement,robot)
-print("Le robot a été placé à ses coordonnées dans l'environnement")
+
 Simulation.afficher_env(environnement)
 for _ in range(3):
-	Simulation.avancer_robot(environnement,robot)
+	Simulation.avancer_robot_env(environnement, robot)
+
+for _ in range(3):
+	Simulation.reculer_robot_env(environnement, robot)
 Simulation.afficher_env(environnement)
+Simulation.distance(robot.x, robot.y, 0, 0)
