2610bd76f7bb2d1bf1cc6156dd5d188f62451fa2
JulienArlais <122969023+JulienArlais@users.noreply.github.com>
2023-01-31 18:58:45 +0100

    Delete Exceptions.py

--- Exceptions.py
+++
@@ -1,7 +0,0 @@
-class InvalidMoveException(Exception):
-    def __init__(self, message):
-        self.message = message
-        
-class TooMuchObstacleException(Exception):
-    def __init__(self, message):
-        self.message = message
