450d472869f589b8da1a87458e6e1673ae0bf173
David Lin <lindavid130@gmail.com>
2023-02-08 10:27:12 +0100

    implémentation du pas de temps dt

--- projet.py
+++ projet.py
@@ -4,6 +4,7 @@ from module.module_outils import format, distance, create_circle
 from module.module_projet import Objet, Robot, Environnement, Roue
 
 mult = 10
+dt = 0.05
 
 class CollisionException(Exception):
 	def __init__(self, message):
@@ -42,16 +43,16 @@ class Simulation:
 		Raises:
 			CollisionException: collision
 		"""
-		self.environnement.avancer_robot(self.robot, 1)
+		self.environnement.avancer_robot(self.robot, dt)
 		rdroite = self.robot.rdroite
-		self.canvas.coords(self.d, self.robot.x*mult, self.robot.y*mult, self.robot.x*mult+4*rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult, self.robot.y*mult+4*rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult) #taille de la flèche : 4
-		self.canvas.move(self.r, rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult, rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult)
+		self.canvas.coords(self.d, self.robot.x*mult, self.robot.y*mult, self.robot.x*mult+rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult, self.robot.y*mult+rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult) #taille de la flèche : 4
+		self.canvas.move(self.r, rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult*dt, rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult*dt)
 		if (self.robot.x+self.robot.rayon > self.environnement.width*self.environnement.scale) or (self.robot.x-self.robot.rayon < 0) or (self.robot.y+self.robot.rayon > self.environnement.height*self.environnement.scale) or (self.robot.y-self.robot.rayon < 0):
 			print("Collision avec les limites de l'environnement")
 			#raise CollisionException("Collision avec les limites de l'environnement")
-			self.environnement.avancer_robot(self.robot, -1)
-			self.canvas.move(self.r, -rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult, -rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult)
-			self.canvas.coords(self.d, self.robot.x*mult, self.robot.y*mult, self.robot.x*mult+4*(-rdroite.vitesse_angulaire*rdroite.rayon)*np.cos(self.robot.theta)*mult, self.robot.y*mult+4*(-rdroite.vitesse_angulaire*rdroite.rayon)*np.sin(self.robot.theta)*mult)
+			self.environnement.avancer_robot(self.robot, -dt)
+			self.canvas.move(self.r, -rdroite.vitesse_angulaire*rdroite.rayon*np.cos(self.robot.theta)*mult*dt, -rdroite.vitesse_angulaire*rdroite.rayon*np.sin(self.robot.theta)*mult*dt)
+			self.canvas.coords(self.d, self.robot.x*mult, self.robot.y*mult, self.robot.x*mult+(-rdroite.vitesse_angulaire*rdroite.rayon)*np.cos(self.robot.theta)*mult, self.robot.y*mult+(-rdroite.vitesse_angulaire*rdroite.rayon)*np.sin(self.robot.theta)*mult)
 			print(self.robot.x,self.robot.y)
 			self.robot.tourner(np.random.uniform(90,270))
 		for objet in self.objets:
@@ -68,13 +69,13 @@ class Simulation:
 			self.update()
 		except CollisionException as e:
 			return
-		self.canvas.after(50, self.run) #vitesse de rafraichissement, argument en millisecondes
+		self.canvas.after((int)(dt*1000), self.run) #vitesse de rafraichissement, argument en millisecondes
 
 
 # Création d'un environnement et d'un robot
 environnement = Environnement(80, 80, 1)
-r_gauche = Roue(90,1)
-r_droite = Roue(90,1)
+r_gauche = Roue(720,1)
+r_droite = Roue(720,1)
 robot = Robot(40, 55.7, 0, 1.6, r_gauche, r_droite)
 
 # Création d'une simulation et ajout du robot et des objets dans l'environnement et affichage de l'environnement
