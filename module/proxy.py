9a98be80b1ba7e34913d2999ea41505879eb81ae
youssef <youssef.el.atia@gmail.com>
2023-03-26 17:30:18 +0200

    Ajout de la documentation pour la classe simulation_proxy

--- module/proxy.py
+++ module/proxy.py
@@ -4,31 +4,70 @@ import numpy as np
 class simulation_proxy:
 
 	def __init__(self, robot, env):
+		"""constructeur de simualtion_proxy,simule les mouvements du robot dans l'environnement virtuel dans lequel il évolue. 
+
+		Args:
+			robot: Robot
+			env: Environnement
+		"""
 		self.robot = robot
 		self.env = env
 		
 	def reset_position(self):
-        	self.robot.last_update = time.time()
+		""" réinitialise la position du robot
+
+		"""
+        self.robot.last_update = time.time()
 
 	def avancer(self, vitesse):
+		"""fait avancer le robot à la vitesse donnée
+
+		Args:
+			vitesse (radian par seconde): vitesse angulaire 
+		"""
+		
 		self.robot.set_vitesse(vitesse, vitesse)
 		
 	def distance_parcourue(self) :
+		"""
+		renvoie la distance parcourue par le robot depuis sa dernière réinitialisation
+
+		"""
+		
 		return self.robot.distance_parcourue()
 
 	def get_distance(self):
+		"""
+		renvoyer la distance entre le robot et un objet de l'environnement virtuel
+		
+		"""
 		return self.robot.get_distance(self.env)
 
 	def get_vitAng(self):
+		"""
+		renvoie la vitesse angulaire du robot
+		"""
 		self.robot.get_vitAng()
 
 	def stop(self):
+		"""
+		arrête le robot en mettant à O la vitesse dans ses deux roues
+		"""
 		self.robot.set_vitesse(0, 0)
 
 	def tourner(self, dps):
+		"""
+		tourne le robot à un certain degré par seconde
+		Args:
+			dps: degré par seconde
+		"""
 		self.robot.tourner(dps)
 
 	def update(self):
+		"""
+		met à jour le robot 
+
+		"""
 		self.robot.update()
 
 class realite_proxy:
