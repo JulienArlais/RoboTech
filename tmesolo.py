faaabe1848e44857249b400aa7a228ca367aa381
David Lin <lindavid130@gmail.com>
2023-04-03 12:25:07 +0200

    Question 2.4

--- tmesolo.py
+++ tmesolo.py
@@ -12,6 +12,10 @@ class StrategieSeqDessine():
 		self.robot = robot
 		
 	def update(self):
+		if (self.indlist%2 == 1):
+			self.robot.dessine(False)
+		else:
+			self.robot.dessine(True)
 		self.liste[self.indlist].update()
 		if self.liste[self.indlist].stop():
 			self.indlist += 1
@@ -21,6 +25,19 @@ class StrategieSeqDessine():
 			self.indlist = 0
 			return True
 		return False
+
+class StrategieBoucle():
+	def __init__(self, strat):
+		self.strat = strat
+		
+	def update(self):
+		self.strat.update()
+			
+	def stop(self):
+		if self.strat.stop():
+			return False
+		return False
+
 
 def q1_1():
 	# Création d'un environnement et d'un robot
@@ -88,5 +105,38 @@ def q2_2():
 	threadrun.start()
 	gui.window.mainloop()
 
+def q2_3():
+	# Création d'un environnement et d'un robot
+	environnement = Environnement(cs.env_width, cs.env_height, cs.scale)
+	robot = Robot(700, 700, -90, cs.rob_r, cs.rob_dist_roue, cs.rob_r_roue) # robot immobile, pour montrer ce que fait le controleur
+	gui = GUI(environnement, robot)
+	proxy_v = Proxy_Virtuel(robot,environnement)
+	# Création d'une simulation
+	s = Simulation(environnement, robot)
+
+	#pour le 1
+	stavance1 = StrategieAvance(200, 1080, proxy_v)
+
+	#Pour le 0
+	stavance2 = StrategieAvance(50, 720, proxy_v)
+	stangle = StrategieAngle(90, 45, proxy_v)
+	liste_strat1 = [stavance2, stangle, stavance1, stangle, stavance2, stangle, stavance1, stangle, stavance2, stangle, stavance1]
+	zero = StrategieSeqDessine(liste_strat1, proxy_v)
+
+	#Pour l'espace
+	liste_strat2 = [stangle, stavance1]
+	espace1 = StrategieSeqDessine(liste_strat2, proxy_v)
+	stangle2 = stangle = StrategieAngle(-90, -45, proxy_v)
+	liste_strat3 = [stangle2, stavance1, stangle2]
+	espace2 = StrategieSeqDessine(liste_strat3, proxy_v)
+
+	liste_strat = [stavance1, espace1, zero, espace2]
+	stseq = StrategieSeqDessine(liste_strat, proxy_v)
+
+	strat_boucle = StrategieBoucle(stseq)
+	threadrun = Thread(target=run, args=(s, gui, strat_boucle))
+	threadrun.start()
+	gui.window.mainloop()
+
 if __name__ == "__main__":
-	q2_2()
+	q2_3()
