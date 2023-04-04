cf89318329c4d275549dc61c0318c879dee6c5cb
Julien Arlais <arlais.julien@gmail.com>
2023-04-04 23:53:25 +0200

    Réorganisation du core et de projet.py

--- module/core.py
+++ module/core.py
@@ -5,3 +5,53 @@ from .controleur.controleur import StrategieAvance, StrategieAngle, StrategieArr
 from .modele.proxy import Proxy_Virtuel, Proxy_Reel
 import module.constante as cs
 import time
+
+stop = False
+lock = Lock()
+
+def run_strategie(strategie):
+	while not strategie.stop():
+		#lock.acquire()
+		strategie.update()
+		#lock.release()
+		time.sleep(cs.dt) # faudrait le changer nan ?
+	global stop
+	stop = True # on arrete le thread de run_simulation lorsque run_strategie se termine
+
+def run_simulation(simulation, gui):
+
+	while not stop:
+		try:
+			#lock.acquire()
+			t0 = time.time()
+			simulation.update()
+			if gui is not None:
+				gui.update()
+			t1 = time.time()
+			if (t1-t0) < cs.dt:
+				time.sleep(cs.dt-(t1-t0))
+			#lock.release()
+		except CollisionException as e:
+			break
+			
+def run(env,rob,prox):
+	# Robot + Environnement
+	environnement = env
+	robot = rob
+	proxy_v = prox
+
+	# Simulation + Interface graphique
+	s = Simulation(environnement, robot)
+	gui = GUI(environnement, robot)
+
+	# Stratégies
+	strat_avance = StrategieAvance(146, 720, proxy_v)
+	strat_angle = StrategieAngle(-90, -45, proxy_v)
+	liste_strat = [strat_avance, strat_angle]*4
+	strat_carre = StrategieSeq(liste_strat)
+
+	t1 = Thread(target=run_simulation, args=(s, gui))
+	t2 = Thread(target=run_strategie, args=(strat_carre,))
+	t1.start()
+	t2.start()
+	gui.window.mainloop()
